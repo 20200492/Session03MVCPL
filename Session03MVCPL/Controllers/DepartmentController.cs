5dd48f3ed78c9053dd1def8916a8b171d41494ca
20200492 <mahmoudhashem20112002@gmail.com>
2024-03-21 16:31:19 +0200

    Department Controller - Create

--- Session03MVCPL/Controllers/DepartmentController.cs
+++ Session03MVCPL/Controllers/DepartmentController.cs
@@ -2,6 +2,7 @@
 using Session03MVCBLL.Interfaces;
 using Session03MVCBLL.Repositories;
 using Session03MVCEDAL.Data;
+using Session03MVCEDAL.Models;
 
 namespace Session03MVCPL.Controllers
 {
@@ -20,5 +21,24 @@ namespace Session03MVCPL.Controllers
             var departments = _departmentRepo.GetAll();
             return View(departments);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid) // Server Side Vaildation
+            {
+                var count = _departmentRepo.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
+        }
+
     }
 }
