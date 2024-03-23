05028ae6f9f13d35c6a62ec525561f2042470ae4
20200492 <mahmoudhashem20112002@gmail.com>
2024-03-23 19:41:03 +0200

    Employee - DAL

---
+++ Session03MVCEDAL/Data/Configrations/EmployeeConfigrations.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Session03MVCEDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03MVCEDAL.Data.Configrations
+{
+    internal class EmployeeConfigrations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            // Fluent APIs for "Employee" Domain
+
+            builder.Property(E => E.Name).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+            builder.Property(E => E.Address).IsRequired();
+
+            builder.Property(E => E.Salary).HasColumnType("decimal(12,2");
+            builder.Property(E => E.Gender)
+                   .HasConversion(
+                    (Gender) => Gender.ToString(),
+                    (genderAsStrin) => (Gender) Enum.Parse(typeof(Gender), genderAsStrin, true)
+                     );
+
+        }
+    }
+
+}
