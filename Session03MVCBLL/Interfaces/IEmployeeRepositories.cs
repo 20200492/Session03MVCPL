0d8ef7afeb1c1f3f812b4e9a8645a9ea3a7c2915
20200492 <mahmoudhashem20112002@gmail.com>
2024-03-24 00:08:07 +0200

    Employee Repository - BLL

---
+++ Session03MVCBLL/Interfaces/IEmployeeRepositories.cs
@@ -0,0 +1,18 @@
+﻿using Session03MVCEDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03MVCBLL.Interfaces
+{
+    internal interface IEmployeeRepositories
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Add(Employee entity);
+        int Update(Employee entity);
+        int Delete(Employee entity);
+    }
+}
---
+++ Session03MVCBLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Session03MVCBLL.Interfaces;
+using Session03MVCEDAL.Data;
+using Session03MVCEDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03MVCBLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepositories
+    {
+        private readonly DbContextApplications _dbcontext; // Null
+
+        /// if we don't use Dependency Injection
+        ///public EmployeeRepository()
+        ///{
+        ///    _dbcontext = new DbContextApplications(new Microsoft.EntityFrameworkCore.DbContextOptions<DbContextApplications>);
+        ///}
+
+        public EmployeeRepository(DbContextApplications dbcontext) // Ask CLR for Creating Object from DbContextApplications
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public IEnumerable<Employee> GetAll()
+           => _dbcontext.Employees.AsNoTracking().ToList();
+        public Employee GetById(int id)
+        {
+            //return _dbcontext.Employees.Find(id);
+            return _dbcontext.Find<Employee>(id); //EF Core New Feature
+
+            ///var Employee = _dbcontext.Employees.Local.Where(D => D.Id == id).FirstOrDefault();
+            ///if(Employee is null)
+            ///   Employee = _dbcontext.Employees.Where(D => D.Id == id).FirstOrDefault();
+        }
+        public int Add(Employee entity)
+        {
+            _dbcontext.Employees.Add(entity);
+            return _dbcontext.SaveChanges();
+        }
+        public int Delete(Employee entity)
+        {
+            _dbcontext.Employees.Remove(entity);
+            return _dbcontext.SaveChanges();
+        }
+        public int Update(Employee entity)
+        {
+            _dbcontext.Employees.Update(entity);
+            return _dbcontext.SaveChanges();
+        }
+    }
+}
