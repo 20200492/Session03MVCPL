a18e25d35e4b12a0c4c6de2d038a5bbfdc762f07
20200492 <mahmoudhashem20112002@gmail.com>
2024-03-27 22:22:32 +0200

    Partial View

--- Session03MVCEDAL/Models/Employee.cs
+++ Session03MVCEDAL/Models/Employee.cs
@@ -18,13 +18,13 @@ namespace Session03MVCEDAL.Models
     public class Employee : ModelBase
     {
         [Required]
-        [MaxLength(50, ErrorMessage = "Max Length of Name is 50 Chars")]
-        [MinLength(50, ErrorMessage = "Min Length of Name is 50 Chars")]
+        [MaxLength(10, ErrorMessage = "Max Length of Name is 50 Chars")]
+        [MinLength(10, ErrorMessage = "Min Length of Name is 10 Chars")]
         public string Name { get; set; }
         [Range(22, 30)]
         public int Age { get; set; }
 
-        [RegularExpression(@"^\d+\s[A-z]+\s[A-z]+$")]
+        //[RegularExpression(@"^\d+\s[A-z]+\s[A-z]+$")]
         public string Address { get; set; }
 
         [DataType(DataType.Currency)]
